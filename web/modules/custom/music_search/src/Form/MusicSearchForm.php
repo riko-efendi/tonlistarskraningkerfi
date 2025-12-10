4f281ccb8ae8f13ddac625f9cd4abcfe9d688df3
Helgi Már J <helgi23@ru.is>
2025-12-10 10:02:30 +0000

    Second search type locked

--- web/modules/custom/music_search/src/Form/MusicSearchForm.php
+++ web/modules/custom/music_search/src/Form/MusicSearchForm.php
@@ -4,6 +4,7 @@ namespace Drupal\music_search\Form;
 
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\TempStore\PrivateTempStoreFactory;
 use Drupal\music_search\MusicSearchService;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
@@ -13,30 +14,41 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
 class MusicSearchForm extends FormBase {
 
   /**
-   * The music search service
+   * The music search service.
+   *
+   * @var \Drupal\music_search\MusicSearchService
    */
   protected $musicSearch;
 
   /**
+   * The tempstore.
+   *
+   * @var \Drupal\Core\TempStore\PrivateTempStore
+   */
+  protected $tempStore;
+
+  /**
    * Constructs a MusicSearchForm object.
    */
-  public function __construct(MusicSearchService $music_search) {
+  public function __construct(MusicSearchService $music_search, PrivateTempStoreFactory $temp_store_factory) {
     $this->musicSearch = $music_search;
+    $this->tempStore = $temp_store_factory->get('music_search');
   }
 
   /**
    * {@inheritdoc}
    */
-  public static function create(ContainerInterface $container) : MusicSearchForm {
+  public static function create(ContainerInterface $container): MusicSearchForm {
     return new static(
-      $container->get('music_search.music_search')
+      $container->get('music_search.music_search'),
+      $container->get('tempstore.private')
     );
   }
 
   /**
    * {@inheritdoc}
    */
-  public function getFormId() : string {
+  public function getFormId(): string {
     return 'music_search_form';
   }
 
@@ -44,11 +56,31 @@ class MusicSearchForm extends FormBase {
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
+    $selections = $this->tempStore->get('selections') ?? [];
+    $selectedAmount = count($selections);
+
+    $isLocked = FALSE;
+    $defaultType = 'artist';
+
+    if ($selectedAmount === 1) {
+      $isLocked = TRUE;
+      $first = reset($selections);
+      if (!empty($first['type'])) {
+        $defaultType = $first['type']; // artist | album | song
+      }
+    }
+
+    if ($isLocked) {
+      $displayType = ucfirst($defaultType);
+    } else {
+      $displayType = 'for ...';
+    }
+
     $form['#attached']['library'][] = 'music_search/music_search';
 
     $form['search'] = [
       '#type' => 'fieldset',
-      '#title' => $this->t('Search Music'),
+      '#title' => $this->t('Search ' . $displayType),
     ];
 
     $form['search']['query'] = [
@@ -63,11 +95,12 @@ class MusicSearchForm extends FormBase {
       '#title' => $this->t('Type'),
       '#options' => [
         'artist' => $this->t('Artist'),
-        'album' => $this->t('Album'),
-        'song' => $this->t('Song'),
+        'album'  => $this->t('Album'),
+        'song'   => $this->t('Song'),
       ],
-      '#default_value' => 'artist',
+      '#default_value' => $defaultType,
       '#required' => TRUE,
+      '#disabled' => $isLocked,
     ];
 
     $form['search']['submit'] = [
@@ -98,33 +131,29 @@ class MusicSearchForm extends FormBase {
   }
 
   /**
-   * AJAX Callback for search
+   * AJAX Callback for search.
    */
   public function ajaxSearchCallback(array &$form, FormStateInterface $form_state) {
     return $form['results'];
   }
 
-
   /**
-   * {@inheritDoc}
+   * {@inheritdoc}
    */
-  public function submitForm(array &$form, FormStateInterface $form_state) : void {
+  public function submitForm(array &$form, FormStateInterface $form_state): void {
     $query = $form_state->getValue('query');
-    $type = $form_state->getValue('type');
+    $type  = $form_state->getValue('type');  // when locked, this will be the locked type
 
     $results = $this->musicSearch->searchAll($query, $type);
 
-    // Use set(), not setValue().
     $form_state->set('results', $results);
     $form_state->setRebuild(TRUE);
   }
 
-
   /**
-   * Render search results
+   * Render search results.
    */
-  protected function renderResults($all_results, FormStateInterface $form_state): string
-  {
+  protected function renderResults($all_results, FormStateInterface $form_state): string {
     if (empty($all_results)) {
       return '<p>' . $this->t('No results found.') . '</p>';
     }
@@ -173,15 +202,6 @@ class MusicSearchForm extends FormBase {
 
         $output .= '<p><strong>' . ucfirst($provider) . ' ID:</strong> ' . htmlspecialchars($item['id']) . '</p>';
 
-        // Add Select button
-        /*$output .= '<button type="button" class="button button--primary select-result"
-        data-provider="' . $provider . '"
-        data-id="' . $item_id . '"
-        data-name="' . htmlspecialchars($item['name']) . '"
-        onclick="selectResult(\'' . $provider . '\', \'' . $item_id . '\', \'' . $type. '\')">';
-        $output .= $this->t('Select This Result');
-        $output .= '</button>';*/
-
         $select_url = '/admin/content/music-search/compare/' . $provider . '/' . $item_id . '/' . $type;
         $output .= '<a href="' . $select_url . '" class="button button--primary" style="margin-top: 10px; display: inline-block;">';
         $output .= $this->t('Select for Comparison');
@@ -196,4 +216,5 @@ class MusicSearchForm extends FormBase {
 
     return $output;
   }
+
 }
